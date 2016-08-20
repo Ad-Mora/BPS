96656ddd668890ace5030deb2e61aa3b62df909f
Adrian Mora <adriandariom@gmail.com>
2016-08-19 22:35:05 -0400

    Created basic view for homepage

--- BPSApp/views.py
+++ BPSApp/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def login_screen(request):
     return render(request, 'BPSApp/Login.html')
+
+
+def homepage(request):
+    return render(request, 'BPSApp/Homepage.html')
