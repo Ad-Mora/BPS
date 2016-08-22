ec97c4bb316b7f1ca69765bc56078ab83a0c6814
Adrian Mora <adriandariom@gmail.com>
2016-08-22 19:45:29 -0400

    Created basic input fields for the customer view

--- BPSApp/views.py
+++ BPSApp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def login_screen(request):
-    return render(request, 'BPSApp/Login.html')
+    return render(request, 'BPSApp/login.html')
 
 
 def homepage(request):
-    return render(request, 'BPSApp/Homepage.html')
+    return render(request, 'BPSApp/homepage.html')
