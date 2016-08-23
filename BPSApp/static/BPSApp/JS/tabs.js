8677c0e20725dcbeba08617f842d75c1b76c4b90
Adrian Mora <adriandariom@gmail.com>
2016-08-22 21:03:17 -0400

    Collected tab pages in tabs javacript file

--- BPSApp/static/BPSApp/JS/tabs.js
+++ BPSApp/static/BPSApp/JS/tabs.js
@@ -1,5 +1,11 @@
 var tabBar = (function(){
 
+    basicInfoPage = document.querySelector("basic-info-page");
+    addressPage = document.querySelector("address-page");
+    contactsPage = document.querySelector("contacts-page");
+    billingPage = document.querySelector("billing-page");
+    notesPage = document.querySelector("notes-page");
+
 
 
 
