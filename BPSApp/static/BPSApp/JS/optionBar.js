3980ad67a441867afc6a64e77219a16ba46d239e
Adrian Mora <adriandariom@gmail.com>
2016-08-24 22:20:11 -0400

    Customer add button displays and hides content appropriately

--- BPSApp/static/BPSApp/JS/optionBar.js
+++ BPSApp/static/BPSApp/JS/optionBar.js
@@ -2,15 +2,27 @@ var optionBar = (function(){
 
     // get DOM objects
     addCustomerButton = document.getElementsByClassName("add-customer-button")[0];
+    cancelCustomerAddButton = document.getElementsByClassName("cancel-customer-add-button")[0];
     customerSearchWrapper = document.getElementsByClassName("search-customer-wrapper")[0];
     tabDisplay = document.getElementsByClassName("tab-display")[0];
 
     // bind events
     addCustomerButton.addEventListener('click', showCustomerFields);
+    cancelCustomerAddButton.addEventListener('click', cancelCustomerAddition);
 
     // functions
     function showCustomerFields(event) {
-        addCustomer
+        addCustomerButton.style.display = "none";
+        customerSearchWrapper.style.display = "none";
+        cancelCustomerAddButton.style.display = "inline-block";
+        tabDisplay.style.display = "block";
+    }
+
+    function cancelCustomerAddition(event) {
+        cancelCustomerAddButton.style.display = "none";
+        tabDisplay.style.display = "none";
+        addCustomerButton.style.display = "inline-block";
+        customerSearchWrapper.style.display = "inline-block";
     }
 
 
