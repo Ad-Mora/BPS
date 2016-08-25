2287fd89ae0196330259aadd2988ee86b3413738
Adrian Mora <adriandariom@gmail.com>
2016-08-25 19:08:10 -0400

    Search drop down list appears and disappears appropriately

--- BPSApp/static/BPSApp/JS/optionBar.js
+++ BPSApp/static/BPSApp/JS/optionBar.js
@@ -1,19 +1,28 @@
 var optionBar = (function(){
 
+    // global variables
+    var searchSuggestionsDisplayed = false;
+
     // get DOM objects
     addCustomerButton = document.getElementsByClassName("add-customer-button")[0];
     cancelCustomerAddButton = document.getElementsByClassName("cancel-customer-add-button")[0];
-    customerSearchWrapper = document.getElementsByClassName("search-customer-wrapper")[0];
+    searchCustomerLabel = document.getElementsByClassName("search-customer-label")[0];
+    searchCustomerFieldWrapper = document.getElementsByClassName("search-customer-field-wrapper")[0];
+    searchCustomerField = searchCustomerFieldWrapper.getElementsByClassName("search-customer-field")[0];
+    searchSuggestions = searchCustomerFieldWrapper.getElementsByClassName("search-suggestions")[0];
     tabDisplay = document.getElementsByClassName("tab-display")[0];
 
     // bind events
     addCustomerButton.addEventListener('click', showCustomerFields);
     cancelCustomerAddButton.addEventListener('click', cancelCustomerAddition);
+    searchCustomerField.addEventListener('focus', toggleSearchSuggestions);
+    searchCustomerField.addEventListener('blur', toggleSearchSuggestions);
 
     // functions
     function showCustomerFields(event) {
         addCustomerButton.style.display = "none";
-        customerSearchWrapper.style.display = "none";
+        searchCustomerLabel.style.display = "none";
+        searchCustomerFieldWrapper.style.display = "none";
         cancelCustomerAddButton.style.display = "inline-block";
         tabDisplay.style.display = "block";
     }
@@ -22,7 +31,18 @@ var optionBar = (function(){
         cancelCustomerAddButton.style.display = "none";
         tabDisplay.style.display = "none";
         addCustomerButton.style.display = "inline-block";
-        customerSearchWrapper.style.display = "inline-block";
+        searchCustomerLabel.style.display = "inline-block";
+        searchCustomerFieldWrapper.style.display = "inline-block";
+    }
+
+    function toggleSearchSuggestions(event) {
+        if (!searchSuggestionsDisplayed) {
+            searchSuggestions.style.display = "block";
+            searchSuggestionsDisplayed = true;
+        } else {
+            searchSuggestions.style.display = "none";
+            searchSuggestionsDisplayed = false;
+        }
     }
 
 
